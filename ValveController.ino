6032b577ad9e7c3278620dc9a1232fc4e0db4ace
Gary Oliver <go@arduinobuilder.aerodesic.net>
2025-08-04 18:44:45 -0700

    Added thread to handle valve open/close logic.

--- ValveController.ino
+++ ValveController.ino
@@ -49,9 +49,56 @@ void setLED(bool value) {
   digitalWrite(led, !value);
 }
 
+TaskHandle_t valveThreadHandle;
+QueueHandle_t valveCommandQueue;
+
+#define OPEN_PIN    21
+#define CLOSE_PIN   22
+#define POWER_PIN   23
+
+// Valve handle thread - this thread never ends
+void valveThread(void *) {
+  for (;;) {
+    // Wait for a message on queue
+    uint8_t command;
+
+    // Attempt to receive an integer from myQueue, waiting for a maximum of 10 ticks if the queue is empty
+    if (xQueueReceive(valveCommandQueue, &command, 10) == pdPASS)
+    {
+      // Item successfully received, process command
+      if (command == 0) {
+        // Close valve
+        digitalWrite(POWER_PIN, HIGH);
+        digitalWrite(CLOSE_PIN, HIGH);
+        delay(5000);
+        digitalWrite(CLOSE_PIN, LOW);
+        digitalWrite(POWER_PIN, LOW);
+    
+      } else {
+        // Open valve
+        digitalWrite(POWER_PIN, HIGH);
+        digitalWrite(OPEN_PIN, HIGH);
+        delay(5000);
+        digitalWrite(OPEN_PIN, LOW);
+        digitalWrite(POWER_PIN, LOW);
+      }
+    }
+    else
+    {
+        // No item received within the specified timeout, or queue was empty
+      delay(50);
+    }
+  }
+}
+
 void valveChanged(bool value) {
   setLED(value);
   log_v("valveChanged: %s, on_time %d\n", value ? "true" : "false", zbValve.getOnTime());
+  // Send command to thread
+  uint8_t command = value ? 1 : 0;
+  if( xQueueSend(valveCommandQueue, &command, (TickType_t) 10) != pdPASS) {
+    log_e("valveChange could not send to queue!");
+  }
 }
 
 /********************* Arduino functions **************************/
@@ -62,6 +109,14 @@ void setup() {
   pinMode(led, OUTPUT);
   digitalWrite(led, LOW);
 
+  // Set mode for valve control pins
+  pinMode(POWER_PIN, OUTPUT);
+  digitalWrite(POWER_PIN, LOW);
+  pinMode(OPEN_PIN, OUTPUT);
+  digitalWrite(OPEN_PIN, LOW);
+  pinMode(CLOSE_PIN, OUTPUT);
+  digitalWrite(CLOSE_PIN, LOW);
+
   // Init button for factory reset
   pinMode(button, INPUT_PULLUP);
 
@@ -69,44 +124,62 @@ void setup() {
   zbValve.setManufacturerAndModel("Aerodesic", "Valve");
 
   // Notify it is battery powered
-  //zbValve.setPowerSource(ZB_POWER_SOURCE_BATTERY, 100, 35);
-  zbValve.setPowerSource(ZB_POWER_SOURCE_MAINS, 100, 35);
+  zbValve.setPowerSource(ZB_POWER_SOURCE_BATTERY, 100, 35);
+  //zbValve.setPowerSource(ZB_POWER_SOURCE_MAINS, 100, 35);
   
   // Set callback function for light change
   zbValve.onValveChanged(valveChanged);
 
-  //Add endpoint to Zigbee Core
-  log_v("Adding ValveController endpoint to Zigbee Core");
-  Zigbee.addEndpoint(&zbValve);
-
-#if 1
-  // Enabling external antenna
-  pinMode(3, OUTPUT);
-  digitalWrite(3, LOW);//turn on this function
-  delay(100);
-  pinMode(14, OUTPUT);
-  digitalWrite(14, HIGH);//use external antenna
-  log_v("External antenna enabled");
-#endif
-
-  //Open network for 180 seconds after boot
-  //Zigbee.setRebootOpenNetwork(180);
-
-  // When all EPs are registered, start Zigbee. By default acts as ZIGBEE_END_DEVICE
-  if (!Zigbee.begin()) {
-    log_e("Zigbee failed to start!");
+  // Start a thread to manage the opening and closing of the valve
+  valveCommandQueue = xQueueCreate(10, sizeof( uint8_t ));
+  if (valveCommandQueue == NULL) {
+    log_e("Valve control control queue could not be created!");
     log_e("Rebooting...");
     ESP.restart();
-  }
 
-  log_v("Connecting to network");
+  } else if (xTaskCreate(valveThread, "ValveControl", 10000, NULL, 0, &valveThreadHandle) != pdPASS) {
+    // Failed - reboot and try again
+    log_e("Valve control thread failed to start!");
+    log_e("Rebooting...");
+    ESP.restart();
+  } else {
+    // Send command to close the valve
+    uint8_t close_command = 0;
+    xQueueSend(valveCommandQueue, &close_command, (TickType_t) 10);
 
-  while (!Zigbee.connected()) {
-    Serial.write('.');
+    //Add endpoint to Zigbee Core
+    log_v("Adding ValveController endpoint to Zigbee Core");
+    Zigbee.addEndpoint(&zbValve);
+
+#if 0
+    // Enabling external antenna
+    pinMode(3, OUTPUT);
+    digitalWrite(3, LOW);//turn on this function
     delay(100);
-  }
+    pinMode(14, OUTPUT);
+    digitalWrite(14, HIGH);//use external antenna
+    log_v("External antenna enabled");
+#endif
 
-  Serial.println();
+    //Open network for 180 seconds after boot
+    //Zigbee.setRebootOpenNetwork(180);
+
+    // When all EPs are registered, start Zigbee. By default acts as ZIGBEE_END_DEVICE
+    if (!Zigbee.begin()) {
+      log_e("Zigbee failed to start!");
+      log_e("Rebooting...");
+      ESP.restart();
+    }
+
+    log_v("Connecting to network");
+
+    while (!Zigbee.connected()) {
+      Serial.write('.');
+      delay(100);
+    }
+
+    Serial.println();
+  }
 }
 
 void loop() {
